449dd1880c33a93b85462af6b77a362a05c01df9
Asami Nakano <nakano.a@pepabo.com>
2015-08-24 14:20:36 +0900

    セルの高さを設定

--- FoodTracker/Classes/Views/Cells/UserTableViewCell.swift
+++ FoodTracker/Classes/Views/Cells/UserTableViewCell.swift
@@ -5,6 +5,8 @@ class UserTableViewCell: UITableViewCell {
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var userIcon: UIImageView!
     
+    // MARK: - Actions
+    
     @IBAction func follow(sender: UIButton) {
     }
 }
