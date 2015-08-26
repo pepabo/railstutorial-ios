436ee8ffefe1edf428bbdd9c5c6d1a21748d6408
takuminnnn <skentaro36@gmail.com>
2015-08-26 15:39:13 +0900

    emailとpasswordが空だとログインボタンが押せないようにした

--- FoodTracker/Classes/Controllers/LoginViewController.swift
+++ FoodTracker/Classes/Controllers/LoginViewController.swift
@@ -8,12 +8,31 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         emailField.delegate = self
         passwordField.delegate = self
+        
+        checkValidLoginForm()
     }
     
+    // MARK: - UITextFieldDelegate
     func textFieldShouldReturn(textField: UITextField) -> Bool{
         view.endEditing(true)
         return false
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        loginButton.enabled = false
+    }
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool{
+        checkValidLoginForm()
+        return true
+    }
+    
+    func checkValidLoginForm() {
+        let email = emailField.text ?? ""
+        let password = passwordField.text ?? ""
+        loginButton.enabled = !email.isEmpty && !password.isEmpty
     }
 }
