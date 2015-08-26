b485e805ef2ad351ff7926e5717d0bd253f9b08b
takuminnnn <skentaro36@gmail.com>
2015-08-26 14:27:29 +0900

    controllerとoutletを接続した
    - email
    - password
    - log in

---
+++ FoodTracker/Classes/Controllers/LoginViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
