682347f88ec4dee04de0c36ae86cdb5c7ce1a39b
takuminnnn <skentaro36@gmail.com>
2015-08-31 18:55:48 +0900

    jsonが空だったときは何もしない

--- FoodTracker/Classes/Controllers/FeedViewController.swift
+++ FoodTracker/Classes/Controllers/FeedViewController.swift
@@ -18,24 +18,27 @@ class FeedViewController: UITableViewController {
     
     func request() {
         Alamofire.request(Router.GetFeed(userId: 1)).responseJSON { (request, response, data, error) -> Void in
-            let json = JSON(data!)
-            println(json)
-            
-            for (index: String, subJson: JSON) in json["contents"] {
-                var picture = ""
-                if let url = subJson["picture"]["url"].string {
-                    picture = url
+            println(data)
+            if data != nil {
+                let json = JSON(data!)
+                println(json)
+                
+                for (index: String, subJson: JSON) in json["contents"] {
+                    var picture = ""
+                    if let url = subJson["picture"]["url"].string {
+                        picture = url
+                    }
+                    var micropost: Micropost = Micropost(
+                        content: subJson["content"].string!,
+                        picture: NSURL(string: picture)
+                    )
+                    self.microposts.set(micropost)
                 }
-                var micropost: Micropost = Micropost(
-                    content: subJson["content"].string!,
-                    picture: NSURL(string: picture)
-                )
-                self.microposts.set(micropost)
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableView!.reloadData()
+                })
             }
-            
-            dispatch_async(dispatch_get_main_queue(), {
-                self.tableView!.reloadData()
-            })
         }
     }
     
