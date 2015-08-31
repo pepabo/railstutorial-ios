e6276e0d5a088111c09aa2d0268912e444bb585a
suzupy <suzuki.n@pepabo.com>
2015-08-31 12:36:59 +0900

    APIを使用してSign Up
    Sign Up後のactivationを促す画面は別タスク
    現在はactivation無しに，200が返った場合はfeedへ飛ぶ

--- FoodTracker/Classes/Controllers/SignupViewController.swift
+++ FoodTracker/Classes/Controllers/SignupViewController.swift
@@ -1,10 +1,19 @@
 import UIKit
+import Alamofire
 
 class SignupViewController: UIViewController, UITextFieldDelegate {
     // MARK: - Properties
     @IBOutlet var textFields: [UITextField]!
+    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmationTextField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordConfirmationLabel: UILabel!
     
     // MARK: - View Events
     override func viewDidLoad() {
@@ -49,5 +58,38 @@ class SignupViewController: UIViewController, UITextFieldDelegate {
             result = result && textField.hasText()
         }
         return result
+    }
+    
+    @IBAction func SubmitSignUpForm(sender: UIButton) {
+        create(nameTextField.text, email: emailTextField.text, password: passwordTextField.text, password_confirmation: confirmationTextField.text)
+    }
+    
+    func create(name: String, email: String, password: String, password_confirmation: String) {
+        let params = [
+            "name": name,
+            "email": email,
+            "password": password,
+            "password_confirmation": password_confirmation
+        ]
+        SVProgressHUD.showWithMaskType(SVProgressHUDMaskType.Black)
+        Alamofire.request(.POST, "https://157.7.190.148/api/users", parameters: params, encoding: .JSON)
+            .responseJSON { (request, response, JSON, error) in
+                println(JSON)
+                var json = JSON as! Dictionary<String, AnyObject>
+                if json["status"] as! Int == 200 {
+                    SVProgressHUD.dismiss()
+                    let targetViewController = self.storyboard!.instantiateViewControllerWithIdentifier("FeedNavigationController") as! UIViewController
+                    self.presentViewController( targetViewController, animated: true, completion: nil)
+                } else{
+                    var messages = json["messages"] as! Dictionary<String, AnyObject>
+                    var user_errors = messages["user"] as! Dictionary<String, String>
+                    println(user_errors)
+                    if let error = user_errors["name"] as String? { self.nameLabel.text = error }
+                    if let error = user_errors["email"] as String? { self.emailLabel.text = error }
+                    if let error = user_errors["password"] as String? { self.passwordLabel.text = error }
+                    if let error = user_errors["password_confirmation"] as String? { self.passwordConfirmationLabel.text = error }
+                    SVProgressHUD.dismiss()
+                }
+        }
     }
 }
