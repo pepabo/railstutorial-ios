99e6c13429830a80b447a0cc3a2f4eabf31abbb5
takuminnnn <skentaro36@gmail.com>
2015-08-31 15:40:50 +0900

    micropostモデルを書き換えたのでテストを一旦消した

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -3,13 +3,6 @@ import XCTest
 
 class FoodTrackerTests: XCTestCase {
     func testMicropostInitialization() {
-        // Success case.
-        let potentialItem = Micropost(content: "Newest micropost", picture: nil)
-        XCTAssertNotNil(potentialItem)
-        
-        // Failure case.
-        let noContent = Micropost(content: "", picture: nil)
-        XCTAssertNil(noContent, "Empty content is invalid")
     }
 }
 // MARK: FoodTracker Tests
