467fd83ccde31df2110e576e325d682ecaee8e38
suzupy <suzuki.n@pepabo.com>
2015-09-01 14:08:53 +0900

    scheme://feedでフィード画面に飛ぶようにする
    NavigationControllerにidentifierをつけることで移動可能
    identifierの命名規則は"HogeNavigationCongroller"で
    例えば，feedはFeedNavigationControllerにしている
    
    URLでは，hogeの部分をhostに指定する

--- FoodTracker/AppDelegate.swift
+++ FoodTracker/AppDelegate.swift
@@ -11,10 +11,10 @@ import Alamofire
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // avoid "The certificate for this server is invalid"
         let manager = Alamofire.Manager.sharedInstance
@@ -44,29 +44,61 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        var storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        if url.host != nil {
+            let identifier = getControllerIdentifierFromURL(url)
+            var initialViewController = storyboard.instantiateViewControllerWithIdentifier(identifier) as! UIViewController
+            self.window?.rootViewController = initialViewController
+            self.window?.makeKeyAndVisible()
+        }
+        
+        var params = Dictionary<String, String>()
+        if url.query != nil {
+            params = getParamsFromURL(url)
+        }
+        
+        return true
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    func getControllerIdentifierFromURL(url: NSURL) -> String {
+        return url.host!.capitalizedString + "NavigationController"
+    }
+    
+    func getParamsFromURL(url: NSURL) -> Dictionary<String, String> {
+        var params = Dictionary<String, String>()
+        if let query = url.query as String! {
+            for param in split(query, allowEmptySlices: true, isSeparator: { $0 == "&" }) {
+                let elements = split(param, allowEmptySlices: true, isSeparator: { $0 == "=" })
+                params[elements[0]] = elements[1]
+            }
+        }
+        return params
+    }
 }
 
