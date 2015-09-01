287092b1da6e78afae760a5f7a56e5d1906f7e9d
takuminnnn <skentaro36@gmail.com>
2015-09-01 15:31:34 +0900

    apiを叩いてjsonを取得する

--- FoodTracker/Classes/Controllers/UserViewController.swift
+++ FoodTracker/Classes/Controllers/UserViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class UserViewController: UITableViewController {
     // MARK: - Properties
@@ -6,6 +8,33 @@ class UserViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.request()
+    }
+    
+    func request() {
+        Alamofire.request(Router.GetAllUsers()).responseJSON { (request, response, data, error) -> Void in
+            println(data)
+            if data != nil {
+                let json = JSON(data!)
+                println(json)
+                
+//                for (index: String, subJson: JSON) in json["contents"] {
+//                    var picture = ""
+//                    if let url = subJson["picture"]["url"].string {
+//                        picture = url
+//                    }
+//                    var micropost: Micropost = Micropost(
+//                        content: subJson["content"].string!,
+//                        picture: NSURL(string: picture)
+//                    )
+//                    self.microposts.set(micropost)
+//                }
+//                
+//                dispatch_async(dispatch_get_main_queue(), {
+//                    self.tableView!.reloadData()
+//                })
+            }
+        }
     }
     
     // MARK: - Table view data source
