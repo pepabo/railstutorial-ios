223dabad0f55301c64fd60798b9bf39e3db94985
takuminnnn <skentaro36@gmail.com>
2015-09-01 15:53:56 +0900

    テストを一旦削除

--- FoodTrackerTests/UserTests.swift
+++ FoodTrackerTests/UserTests.swift
@@ -2,17 +2,4 @@ import UIKit
 import XCTest
 
 class UserTests: XCTestCase {
-
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
-
-    func testUserInitialization() {
-        let user = User(name: "hoge", icon: "")
-        XCTAssertNotNil(user)
-    }
 }
