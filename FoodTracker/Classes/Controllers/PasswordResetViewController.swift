4742b17e0da49cd43867eeb6f6fd836965d3b143
Asami Nakano <nakano.a@pepabo.com>
2015-09-01 14:24:46 +0900

    キーボードを収納するように変更

--- FoodTracker/Classes/Controllers/PasswordResetViewController.swift
+++ FoodTracker/Classes/Controllers/PasswordResetViewController.swift
@@ -1,5 +1,11 @@
 import UIKit
 
 class PasswordResetViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var emailTextField: UITextField!
 
+    // MARK: - Actions
+    @IBAction func unFocusTextField(sender: UITapGestureRecognizer) {
+        emailTextField.resignFirstResponder()
+    }
 }
