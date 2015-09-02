5027e700d95ea0e8adc3996bb0f8775663811a53
takuminnnn <skentaro36@gmail.com>
2015-09-02 16:57:45 +0900

    profileのviewControllerを用意した

---
+++ Kakico/Classes/Controllers/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var userIcon: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var postNum: UIButton!
+    @IBOutlet weak var followingNum: UIButton!
+    @IBOutlet weak var followerNum: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
