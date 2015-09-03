ab81b78123870b9e8fc637cb5fe7935e2b47c9ba
takuminnnn <skentaro36@gmail.com>
2015-09-03 17:07:11 +0900

    新たにMicropostViewControllerを用意してfeedがそれを継承する

--- Kakico/Classes/Controllers/FeedViewController.swift
+++ Kakico/Classes/Controllers/FeedViewController.swift
@@ -3,17 +3,8 @@ import SVProgressHUD
 import Alamofire
 import SwiftyJSON
 
-class FeedViewController: UITableViewController {
-    // MARK: - Properties
-    var microposts = MicropostDataManager()
-    
-    // MARK: - View Events
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        request()
-    }
-
-    func request() {
+class FeedViewController: MicropostViewController {
+    override func request() {
         SVProgressHUD.showWithMaskType(.Black)
         Alamofire.request(Router.GetFeed()).responseJSON { (request, response, data, error) -> Void in
             println(data)
@@ -41,40 +32,5 @@ class FeedViewController: UITableViewController {
                 SVProgressHUD.showErrorWithStatus("", maskType: .Black)
             }
         }
-    }
-    
-    // MARK: - Table view data source
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.microposts.size
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cellIdentifier = "Micropost"
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MicropostCell
-        
-        let micropost = self.microposts[indexPath.row] as Micropost
-
-        cell.contentLabel.text = micropost.content
-        cell.pictureImageView.sd_setImageWithURL(micropost.picture)
-        
-        return cell
-    }
-    
-    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 216
-    }
-    
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return UITableViewAutomaticDimension
-    }
-    
-    // MARK: - Navigation
-    @IBAction func unwindToMicropostList(sender: UIStoryboardSegue) {
-        request()
     }
 }
---
+++ Kakico/Classes/Controllers/MicropostViewController.swift
@@ -0,0 +1,80 @@
+import UIKit
+import SVProgressHUD
+import Alamofire
+import SwiftyJSON
+
+class MicropostViewController: UITableViewController {
+    // MARK: - Properties
+    var microposts = MicropostDataManager()
+
+    // MARK: - View Events
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        request()
+    }
+
+    func request() {
+        SVProgressHUD.showWithMaskType(.Black)
+        Alamofire.request(Router.GetFeed()).responseJSON { (request, response, data, error) -> Void in
+            println(data)
+            if data != nil {
+                let json = JSON(data!)
+                println(json)
+
+                for (index: String, subJson: JSON) in json["contents"] {
+                    var picture = ""
+                    if let url = subJson["picture"]["url"].string {
+                        picture = url
+                    }
+                    var micropost: Micropost = Micropost(
+                        content: subJson["content"].string!,
+                        picture: NSURL(string: picture)
+                    )
+                    self.microposts.set(micropost)
+                }
+
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableView.reloadData()
+                })
+                SVProgressHUD.dismiss()
+            } else {
+                SVProgressHUD.showErrorWithStatus("", maskType: .Black)
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.microposts.size
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "Micropost"
+
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MicropostCell
+
+        let micropost = self.microposts[indexPath.row] as Micropost
+
+        cell.contentLabel.text = micropost.content
+        cell.pictureImageView.sd_setImageWithURL(micropost.picture)
+
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 216
+    }
+
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+
+    // MARK: - Navigation
+    @IBAction func unwindToMicropostList(sender: UIStoryboardSegue) {
+        request()
+    }
+}
