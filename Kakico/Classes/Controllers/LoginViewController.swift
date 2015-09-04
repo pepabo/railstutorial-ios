7e855c78e09d39c05d2a26316f7b996369023e84
Asami Nakano <nakano.a@pepabo.com>
2015-09-04 19:10:46 +0900

    キーボードでテキストフィールドが隠れないようにした

--- Kakico/Classes/Controllers/LoginViewController.swift
+++ Kakico/Classes/Controllers/LoginViewController.swift
@@ -4,11 +4,13 @@ import Alamofire
 import SwiftyJSON
 import KeychainAccess
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
+class LoginViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate {
     // MARK: - Properties
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    var activeTextField = UITextField()
     
     // MARK: - View Events
     override func viewDidLoad() {
@@ -19,7 +21,17 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
         checkValidLoginForm()
     }
-    
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardNotifications()
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        removeKeyboardNotifications()
+        super.viewWillDisappear(animated)
+    }
+
     // MARK: -
     @IBAction func touchLogInButton(sender: UIButton) {
         login(emailField.text, password: passwordField.text)
@@ -31,6 +43,11 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     // MARK: - UITextFieldDelegate
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        activeTextField = textField
+        return true
+    }
+
     func textFieldShouldReturn(textField: UITextField) -> Bool{
         view.endEditing(true)
         if textField.tag == 1 {
@@ -38,7 +55,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         }
         return false
     }
-    
+
     func textFieldDidBeginEditing(textField: UITextField) {
         loginButton.enabled = false
     }
@@ -62,7 +79,36 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     func checkPresenceField(email: String, password: String) -> Bool{
         return !email.isEmpty && !password.isEmpty
     }
-    
+
+    // MARK: - Keyboard
+    func addKeyboardNotifications() {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "handleKeyboardWillShowNotification:", name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "handleKeyboardWillHideNotification:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+
+    func removeKeyboardNotifications() {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+
+    func handleKeyboardWillShowNotification(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let myBoundSize: CGSize = UIScreen.mainScreen().bounds.size
+        var txtLimit = activeTextField.frame.origin.y + activeTextField.frame.height + 8.0
+        let kbdLimit = myBoundSize.height - keyboardScreenEndFrame.size.height
+
+        if txtLimit >= kbdLimit {
+            scrollView.contentOffset.y = txtLimit - kbdLimit
+        }
+    }
+
+    func handleKeyboardWillHideNotification(notification: NSNotification) {
+        scrollView.contentOffset.y = 0
+    }
+
     // MARK: -
     func login(email: String, password: String) {
         let params = [
