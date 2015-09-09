0b41a04596d6145c470ec18c94c5fe1eb527e433
suzupy <suzuki.n@pepabo.com>
2015-09-09 18:41:11 +0900

    ProfileEditViewControllerとProfile Editを繋ぐ
    また，Sign Upも合わせて修正

--- Kakico/Classes/Controllers/ProfileEditViewController.swift
+++ Kakico/Classes/Controllers/ProfileEditViewController.swift
@@ -4,18 +4,32 @@ import SVProgressHUD
 import SwiftyJSON
 import KeychainAccess
 
-class ProfileEditViewController: UIViewController {
-    
+class ProfileEditViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate {
+                                                  
     // MARK: - Properties
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmationTextField: UITextField!
+    @IBOutlet var textFields: [UITextField]!
+    private var activeTextField = UITextField()
+    private var keyboardLimit: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initForm()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardNotifications()
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        removeKeyboardNotifications()
+        super.viewWillDisappear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +38,13 @@ class ProfileEditViewController: UIViewController {
     }
     
     // MARK: - Actions
-    
+
+    @IBAction func unFocusTextField(sender: UITapGestureRecognizer) {
+        for textField : UITextField in textFields {
+            textField.resignFirstResponder()
+        }
+    }
+
     @IBAction func submitProfileEditForm(sender: UIButton) {
         submit(nameTextField.text, email: emailTextField.text, password: passwordTextField.text, password_confirmation: confirmationTextField.text)
     }
@@ -66,7 +86,70 @@ class ProfileEditViewController: UIViewController {
             }
         }
     }
+
+    // MARK: - UITextFieldDelegate
+
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        activeTextField = textField
+        return true
+    }
+
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        return true
+    }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField.tag == 4 {
+            textField.resignFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            for field : UITextField in textFields {
+                if field.tag == textField.tag + 1 {
+                    field.becomeFirstResponder()
+                }
+            }
+        }
+        return false
+    }
+    
+    func checkPresenceField() -> Bool{
+        var result = true
+        for textField : UITextField in textFields {
+            result = result && textField.hasText()
+        }
+        return result
+    }
+
+    // MARK: - Keyboard
+
+    func addKeyboardNotifications() {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "handleKeyboardWillShowNotification:", name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "handleKeyboardWillHideNotification:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+
+    func removeKeyboardNotifications() {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+
+    func handleKeyboardWillShowNotification(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let myBoundSize: CGSize = UIScreen.mainScreen().bounds.size
+        var navigationBarHeight = self.navigationController!.navigationBar.frame.size.height
+        var txtLimit = activeTextField.frame.origin.y + activeTextField.frame.height + 30.0 + navigationBarHeight
+        keyboardLimit = myBoundSize.height - keyboardScreenEndFrame.size.height
+        if txtLimit >= keyboardLimit {
+            scrollView.contentOffset.y = txtLimit - keyboardLimit!
+        }
+    }
+
+    func handleKeyboardWillHideNotification(notification: NSNotification) {
+        scrollView.contentOffset.y = 0
+    }
+
     /*
     // MARK: - Navigation
 
