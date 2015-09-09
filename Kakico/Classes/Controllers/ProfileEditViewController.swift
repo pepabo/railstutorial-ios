3664ee9c2dbc744685545f7aacc9e2d2b93347c9
suzupy <suzuki.n@pepabo.com>
2015-09-09 10:47:57 +0900

    Add ProfileEditViewController

---
+++ Kakico/Classes/Controllers/ProfileEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileEditViewController.swift
+//  Kakico
+//
+//  Created by usr0600370 on 2015/09/08.
+//  Copyright (c) 2015年 usr0600370. All rights reserved.
+//
+
+import UIKit
+
+class ProfileEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
