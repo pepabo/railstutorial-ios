eaebce14984b022f94476b529b7b8e1ef7765ec9
suzupy <suzuki.n@pepabo.com>
2015-09-09 12:39:37 +0900

    空白削除

--- Kakico/Classes/Controllers/ProfileEditViewController.swift
+++ Kakico/Classes/Controllers/ProfileEditViewController.swift
@@ -17,7 +17,7 @@ class ProfileEditViewController: UIViewController {
         super.viewDidLoad()
         initForm()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -69,12 +69,12 @@ class ProfileEditViewController: UIViewController {
     
     /*
     // MARK: - Navigation
-    
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     // Get the new view controller using segue.destinationViewController.
     // Pass the selected object to the new view controller.
     }
     */
-    
+
 }
