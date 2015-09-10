441bfc8506f5a73b4638897b514e7e0cc9d4d8db
takuminnnn <skentaro36@gmail.com>
2015-09-10 16:17:26 +0900

    リセットメールを送れる
    送るのに成功したらログイン画面に戻るようにした

--- Kakico/Classes/Controllers/ForgotPasswordViewController.swift
+++ Kakico/Classes/Controllers/ForgotPasswordViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import Alamofire
+import SwiftyJSON
 import SVProgressHUD
 
 class ForgotPasswordViewController: UIViewController, UITextFieldDelegate {
@@ -12,7 +14,7 @@ class ForgotPasswordViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: - Actions
     @IBAction func touchSubmitButton(sender: UIButton) {
-        forgotPassword()
+        sendResetEmail(emailTextField.text)
     }
 
     @IBAction func unFocusTextField(sender: UITapGestureRecognizer) {
@@ -27,13 +29,29 @@ class ForgotPasswordViewController: UIViewController, UITextFieldDelegate {
     }
 
     // MARK: -
-    private func forgotPassword() {
+    func moveToLoginView() {
+        let loginView = self.storyboard!.instantiateViewControllerWithIdentifier("LoginViewController") as! UIViewController
+        loginView.modalTransitionStyle = UIModalTransitionStyle.CoverVertical
+        self.presentViewController(loginView, animated: true, completion: nil)
+    }
+
+    func sendResetEmail(email: String) {
+        let params = [
+            "email": email
+        ]
+
         emailTextField.resignFirstResponder()
-        SVProgressHUD.showWithStatus("", maskType: .Black)
-        if checkValidForm() {
-            SVProgressHUD.showSuccessWithStatus("", maskType: .Black)
-        } else {
-            SVProgressHUD.showErrorWithStatus("", maskType: .Black)
+        SVProgressHUD.showWithMaskType(SVProgressHUDMaskType.Black)
+
+        Alamofire.request(Router.PostPasswordResetEmail(params: params)).responseJSON { (request, response, data, error) -> Void in
+            let json = JSON(data!)
+            println(json)
+            println(json["status"])
+            if json["status"] == 200 {
+                self.moveToLoginView()
+            } else{
+                SVProgressHUD.showErrorWithStatus(json["messages"]["notice"][0].stringValue)
+            }
         }
     }
 
--- Kakico/Classes/Models/Router.swift
+++ Kakico/Classes/Models/Router.swift
@@ -19,6 +19,9 @@ enum Router: URLRequestConvertible {
     
     case PostRelationships(followedId: Int)
     case DeleteRelationships(followedId: Int)
+
+    case PostPasswordResetEmail(params: Dictionary<String, String>)
+    case UpdatePassword(params: Dictionary<String, String>)
     
     var method: Alamofire.Method {
         switch self {
@@ -33,6 +36,8 @@ enum Router: URLRequestConvertible {
         case .PostMicropost: return .POST
         case .PostRelationships: return .POST
         case .DeleteRelationships: return .DELETE
+        case .PostPasswordResetEmail: return .POST
+        case .UpdatePassword: return .POST
         }
     }
     
@@ -52,6 +57,9 @@ enum Router: URLRequestConvertible {
         case .PostMicropost: return "/api/microposts/post"
         case .PostRelationships(let followedId): return "/api/relationships/\(followedId)"
         case .DeleteRelationships(let followedId): return "/api/relationships/\(followedId)"
+
+        case .PostPasswordResetEmail: return "/api/password_resets/create"
+        case .UpdatePassword: return "/api/password_resets/update"
         }
     }
 
@@ -82,6 +90,10 @@ enum Router: URLRequestConvertible {
             return Alamofire.ParameterEncoding.URL.encode(mutableURLRequest, parameters: parameters).0
         case .GetMicroposts(let userId, let parameters):
             return Alamofire.ParameterEncoding.URL.encode(mutableURLRequest, parameters: parameters).0
+        case .PostPasswordResetEmail(let parameters):
+            return Alamofire.ParameterEncoding.JSON.encode(mutableURLRequest, parameters: parameters).0
+        case .UpdatePassword(let parameters):
+            return Alamofire.ParameterEncoding.JSON.encode(mutableURLRequest, parameters: parameters).0
         default:
             return mutableURLRequest
         }
