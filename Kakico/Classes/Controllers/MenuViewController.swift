58e80b8f84b028fc2aeafc6fdc5aeb0e9ffd0f62
takuminnnn <skentaro36@gmail.com>
2015-09-10 11:13:55 +0900

    確認アラートを出してログアウトするか選択できるようにした
    - storyboardのsegueをやめてコードからログイン画面に遷移するようにした
    - ログアウトのセルが押されたかどうかはセクション数で判定してる

--- Kakico/Classes/Controllers/MenuViewController.swift
+++ Kakico/Classes/Controllers/MenuViewController.swift
@@ -7,10 +7,47 @@ class MenuViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-    func logout() {
+    func removeKeychain() {
         var keychain = Keychain(service: "nehan.Kakico")
         keychain["authToken"] = nil
         keychain["userId"] = nil
+        println("---------- keychain info ----------")
+        println(keychain["authToken"])
+        println(keychain["userId"])
+        println("---------- keychain info ----------")
+    }
+
+    func moveToLoginView() {
+        let loginView = self.storyboard!.instantiateViewControllerWithIdentifier("LoginViewController") as! UIViewController
+        loginView.modalTransitionStyle = UIModalTransitionStyle.CoverVertical
+        self.presentViewController(loginView, animated: true, completion: nil)
+    }
+
+    func logout() {
+        removeKeychain()
+        moveToLoginView()
+        println("Log out complete...")
+    }
+
+    func selectAlert() {
+        let alertController = UIAlertController(title: "Are you sure you want to log out?", message: "", preferredStyle: .ActionSheet)
+        let logoutAction = UIAlertAction(title: "Yes", style: .Default) {
+            action in self.logout()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) {
+            action in println("Cancel")
+        }
+
+        alertController.addAction(logoutAction)
+        alertController.addAction(cancelAction)
+
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 2 {
+            selectAlert()
+        }
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
@@ -26,8 +63,6 @@ class MenuViewController: UITableViewController {
             default:
                 println("undefined segue.identifier in ConfigViewController")
             }
-        }else if segue.identifier == "Logout" {
-            logout()
         }
     }
 }
