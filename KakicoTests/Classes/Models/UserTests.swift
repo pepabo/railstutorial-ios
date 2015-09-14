721353cb0fe354324baec1a2bbe5db7497da37ec
Asami Nakano <nakano.a@pepabo.com>
2015-09-14 12:56:17 +0900

    User作成のテストの第一歩をつくった

--- KakicoTests/Classes/Models/UserTests.swift
+++ KakicoTests/Classes/Models/UserTests.swift
@@ -10,7 +10,29 @@ class UserTests: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
-        XCTAssert(true, "Pass")
+    func createUser() {
+        let contents = [
+            "created_at" : "2015-08-24T09:59:06.000Z",
+            "email" : "test@test.com",
+            "followers_count" : 10,
+            "following_count" : 20,
+            "following_flag" : 1,
+            "icon_url" : "https://secure.gravatar.com/avatar",
+            "id" : 1,
+            "microposts_count" : 51,
+            "name" : "hoge",
+            "unix_time_created_at" : 1440410346,
+            "unix_time_updated_at" : 1441939154,
+            "updated_at" : "2015-09-11T02:39:14.000Z",
+        ]
+
+        let icon = NSURL(string: contents["icon_url"] as! String)!
+        let user = User(
+            id: contents["id"] as! Int,
+            name: contents["name"] as! String,
+            icon: icon,
+            followingFlag: contents["following_flag"] as! Bool
+        )
+        XCTAssertEqual(user.id, contents["id"] as! Int)
     }
 }
