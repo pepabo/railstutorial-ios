59844b786b1740a7a4eb276882a071d3cad7c1fb
Asami Nakano <nakano.a@pepabo.com>
2015-09-14 16:53:05 +0900

    GetUser用のテストを作った

--- Kakico/Classes/Models/APIClient.swift
+++ Kakico/Classes/Models/APIClient.swift
@@ -1,4 +1,5 @@
 import Alamofire
+import SwiftyJSON
 
 class APIClient: NSObject {
     func getUser(
@@ -6,7 +7,18 @@ class APIClient: NSObject {
         onSuccess: (user: User) -> Void,
         onFailure: (error: NSHTTPURLResponse) -> Void
         ) {
-
-
-    }
+            Alamofire.request(Router.GetUser(userId: userId)).responseJSON { (request, response, data, error) -> Void in
+                if data == nil {
+                    onFailure(error: response!)
+                } else {
+                    let json = JSON(data!)
+                    switch json["status"] {
+                    case 200:
+                        onSuccess(user: User(data: json["contents"]))
+                    default:
+                        onFailure(error: response!)
+                    }
+                }
+            }
+        }
 }
--- KakicoTests/Classes/Models/APIClientTests.swift
+++ KakicoTests/Classes/Models/APIClientTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 
 class APIClientTests: XCTestCase {
-
+    let apiClient = APIClient()
     override func setUp() {
         super.setUp()
     }
@@ -11,21 +11,40 @@ class APIClientTests: XCTestCase {
     }
 
 
-    func testGetUser() {
-        let expectation = expectationWithDescription("getUser")
+    func testGetUserSuccess() {
+        let expectation = expectationWithDescription("getUserSuccess")
 
         var sample: User? = nil
-        let apiClient = APIClient()
+
         let userId = 1
         apiClient.getUser(userId,
             onSuccess: { (user) -> Void in
                 sample = user
                 expectation.fulfill()
             }, onFailure: { (error) -> Void in
+                expectation.fulfill()
         })
 
         waitForExpectationsWithTimeout(5.0, handler: nil)
 
         XCTAssertEqual(sample!.id, userId)
+    }
+
+    func testGetUserFailure() {
+        let expectation = expectationWithDescription("getUserFailure")
+
+        var sample: User? = nil
+        let userId = 999999
+        apiClient.getUser(userId,
+            onSuccess: { (user) -> Void in
+                sample = user
+                expectation.fulfill()
+            }, onFailure: { (error) -> Void in
+                expectation.fulfill()
+        })
+
+        waitForExpectationsWithTimeout(5.0, handler: nil)
+
+        XCTAssertTrue(sample == nil)
     }
 }
