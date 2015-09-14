bf708b3294743bde8bad1376e9314c91074c751f
Asami Nakano <nakano.a@pepabo.com>
2015-09-14 15:28:29 +0900

    GetUser用のテストを追加

--- Kakico/Classes/Models/APIClient.swift
+++ Kakico/Classes/Models/APIClient.swift
@@ -1,3 +1,12 @@
-class APIClient {
-    
+import Alamofire
+
+class APIClient: NSObject {
+    func getUser(
+        userId: Int,
+        onSuccess: (user: User) -> Void,
+        onFailure: (error: NSHTTPURLResponse) -> Void
+        ) {
+
+
+    }
 }
--- KakicoTests/Classes/Models/APIClientTests.swift
+++ KakicoTests/Classes/Models/APIClientTests.swift
@@ -10,7 +10,22 @@ class APIClientTests: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
-        XCTAssert(true, "Pass")
+
+    func testGetUser() {
+        let expectation = expectationWithDescription("getUser")
+
+        var sample: User? = nil
+        let apiClient = APIClient()
+        let userId = 1
+        apiClient.getUser(userId,
+            onSuccess: { (user) -> Void in
+                sample = user
+                expectation.fulfill()
+            }, onFailure: { (error) -> Void in
+        })
+
+        waitForExpectationsWithTimeout(5.0, handler: nil)
+
+        XCTAssertEqual(sample!.id, userId)
     }
 }
