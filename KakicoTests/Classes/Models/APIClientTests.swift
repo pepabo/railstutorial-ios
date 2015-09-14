eb4b0747a006627dcc549600d903d89637bfe6f0
Asami Nakano <nakano.a@pepabo.com>
2015-09-14 12:06:55 +0900

    APIClientを作成

---
+++ KakicoTests/Classes/Models/APIClientTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+class APIClientTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+}
