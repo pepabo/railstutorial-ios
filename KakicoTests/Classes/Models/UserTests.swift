a83fc349e98dd9bdaa86b9af1d923afd00e840ae
Asami Nakano <nakano.a@pepabo.com>
2015-09-14 12:16:03 +0900

    UserTestsを追加

---
+++ KakicoTests/Classes/Models/UserTests.swift
@@ -0,0 +1,16 @@
+import UIKit
+import XCTest
+
+class UserTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        XCTAssert(true, "Pass")
+    }
+}
