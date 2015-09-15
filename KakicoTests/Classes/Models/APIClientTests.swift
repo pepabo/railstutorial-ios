7f3bf30505db26eeadcaca9decf1f15e5348cd2c
Asami Nakano <nakano.a@pepabo.com>
2015-09-15 18:19:07 +0900

    FixturesにJSONデータを分離

--- KakicoTests/Classes/Models/APIClientTests.swift
+++ KakicoTests/Classes/Models/APIClientTests.swift
@@ -4,28 +4,13 @@ import OHHTTPStubs
 class APIClientTests: XCTestCase {
     let apiClient = APIClient()
 
+    override func setUp() {
+        super.setUp()
+
+    }
+
     func testGetUserSuccess() {
         let userId = 1
-        let response = [
-            "contents": [
-                "created_at" : "2015-08-24T09:59:06.000Z",
-                "email": "test@test.com",
-                "followers_count": 5,
-                "following_count": 10,
-                "icon_url": "https://secure.gravatar.com/avatar/",
-                "id": 1,
-                "microposts_count": 50,
-                "name": "test",
-                "following_flag": false,
-                "unix_time_created_at": 1440410346,
-                "unix_time_updated_at": 1441939154,
-                "updated_at": "2015-09-11T02:39:14.000Z"
-            ],
-            "messages": [
-                "notice": []
-            ],
-            "status": 200
-        ]
 
         OHHTTPStubs.stubRequestsPassingTest(
             { (request: NSURLRequest) -> Bool in
@@ -34,11 +19,11 @@ class APIClientTests: XCTestCase {
                 }
                 return false
             }, withStubResponse: { (request: NSURLRequest) -> OHHTTPStubsResponse in
-                return OHHTTPStubsResponse(JSONObject: response, statusCode:200, headers:nil)
+                return OHHTTPStubsResponse(fileAtPath:OHPathForFile("user.json", self.dynamicType)!,
+                    statusCode:200, headers:["Content-Type":"application/json"])
         })
 
         let expectation = expectationWithDescription("getUserSuccess")
-
 
         apiClient.getUser(userId,
             onSuccess: { (user) -> Void in
