a588686702b243597e8b161d50baebedbce4f3ba
takuminnnn <skentaro36@gmail.com>
2015-09-15 16:19:50 +0900

    プロフィールからとりあえず各リストへ遷移する
    ただし遷移してもリストは自分のものが表示される

--- Kakico/Classes/Controllers/ProfileHeaderViewController.swift
+++ Kakico/Classes/Controllers/ProfileHeaderViewController.swift
@@ -84,4 +84,18 @@ class ProfileHeaderViewController: UIViewController {
         button.setTitle("Follow", forState: .Normal)
         button.setTitleColor(UIColor.DefaultColor(), forState: .Normal)
     }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if let userView = segue.destinationViewController as? UserViewController {
+            let type = segue.identifier!
+            switch type {
+            case "Followers":
+                userView._listType = "Followers"
+            case "Following":
+                userView._listType = "Following"
+            default:
+                println("undefined segue.identifier in ConfigViewController")
+            }
+        }
+    }
 }
