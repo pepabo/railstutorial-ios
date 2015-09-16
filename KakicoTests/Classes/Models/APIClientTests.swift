1b584949cc0813e28b4b29d9696f1138fc1cf156
Asami Nakano <nakano.a@pepabo.com>
2015-09-16 10:50:07 +0900

    APIStubを各APIによって分岐させるように修正

--- KakicoTests/Classes/Models/APIClientTests.swift
+++ KakicoTests/Classes/Models/APIClientTests.swift
@@ -3,31 +3,14 @@ import OHHTTPStubs
 
 class APIClientTests: XCTestCase {
     let apiClient = APIClient()
-
-    override func setUp() {
-        super.setUp()
-
-    }
+    let apiStub = APIStub()
 
     func testGetUserSuccess() {
-        let userId = 1
-
-        OHHTTPStubs.stubRequestsPassingTest(
-            { (request: NSURLRequest) -> Bool in
-                if request.URL?.absoluteString == "https://157.7.190.148/api/users/" + String(userId) {
-                    return true
-                }
-                return false
-            }, withStubResponse: { (request: NSURLRequest) -> OHHTTPStubsResponse in
-                return OHHTTPStubsResponse(fileAtPath:OHPathForFile("user.json", self.dynamicType)!,
-                    statusCode:200, headers:["Content-Type":"application/json"])
-        })
-
         let expectation = expectationWithDescription("getUserSuccess")
 
-        apiClient.getUser(userId,
+        apiClient.getUser(apiStub.userId,
             onSuccess: { (user) -> Void in
-                XCTAssertEqual(user.id, userId)
+                XCTAssertEqual(user.id, self.apiStub.userId)
                 expectation.fulfill()
             }, onFailure: { (error) -> Void in
                 XCTFail()
@@ -35,19 +18,18 @@ class APIClientTests: XCTestCase {
 
         waitForExpectationsWithTimeout(5.0, handler: nil)
     }
-//
-//    func testGetUserFailure() {
-//        let expectation = expectationWithDescription("getUserFailure")
-//
-//        let userId = 999999
-//        apiClient.getUser(userId,
-//            onSuccess: { (user) -> Void in
-//                XCTFail()
-//            }, onFailure: { (error) -> Void in
-//                XCTAssertNotNil(error)
-//                expectation.fulfill()
-//        })
-//
-//        waitForExpectationsWithTimeout(5.0, handler: nil)
-//    }
+
+    func testGetUserFailure() {
+        let expectation = expectationWithDescription("getUserFailure")
+
+        apiClient.getUser(apiStub.illegalValue,
+            onSuccess: { (user) -> Void in
+                XCTFail()
+            }, onFailure: { (error) -> Void in
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+        })
+
+        waitForExpectationsWithTimeout(5.0, handler: nil)
+    }
 }
---
+++ KakicoTests/Helpers/APIStub.swift
@@ -0,0 +1,27 @@
+import OHHTTPStubs
+
+class APIStub {
+    let userId = 1
+    let illegalValue = 999999
+
+    init() {
+        OHHTTPStubs.stubRequestsPassingTest(
+            { (request: NSURLRequest) -> Bool in
+                if request.URL!.path!.hasSuffix("/api/users/feed") {
+                    return false
+                }
+                if request.URL != nil && Router.baseURLString.hasSuffix(request.URL!.host!) {
+                    return true
+                }
+                return false
+            }, withStubResponse: { (request: NSURLRequest) -> OHHTTPStubsResponse in
+                println(request.URL!.path!)
+                switch request.URL!.path! {
+                case "/api/users/\(self.userId)": return OHHTTPStubsResponse(fileAtPath:OHPathForFile("user.json", self.dynamicType)!,
+                    statusCode:200, headers:["Content-Type":"application/json"])
+                default: return OHHTTPStubsResponse(fileAtPath:OHPathForFile("500.json", self.dynamicType)!,
+                    statusCode:500, headers:["Content-Type":"application/json"])
+                }
+        })
+    }
+}
