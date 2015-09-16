80c168458ee3d4f262df80952a50f19052bfe233
suzupy <suzuki.n@pepabo.com>
2015-09-16 10:49:11 +0900

    Fix followButton's warning

--- Kakico/Classes/Views/Cells/UserTableViewCell.swift
+++ Kakico/Classes/Views/Cells/UserTableViewCell.swift
@@ -4,5 +4,5 @@ class UserTableViewCell: UITableViewCell {
     
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var userIcon: UIImageView!
-    @IBOutlet weak var followButton: UIButton!    
+    @IBOutlet weak var followUserButton: followButton!
 }
