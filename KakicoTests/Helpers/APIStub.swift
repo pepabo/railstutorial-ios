604f5610b107ef2ff602ceb1aae5043376c0fe12
Asami Nakano <nakano.a@pepabo.com>
2015-09-16 11:01:09 +0900

    メソッドに抽出

--- KakicoTests/Helpers/APIStub.swift
+++ KakicoTests/Helpers/APIStub.swift
@@ -7,10 +7,7 @@ class APIStub {
     init() {
         OHHTTPStubs.stubRequestsPassingTest(
             { (request: NSURLRequest) -> Bool in
-                if request.URL != nil && Router.baseURLString.hasSuffix(request.URL!.host!) {
-                    return true
-                }
-                return false
+                return self.checkHost(request)
             }, withStubResponse: { (request: NSURLRequest) -> OHHTTPStubsResponse in
                 switch request.URL!.path! {
                 case "/api/users/\(self.userId)": return OHHTTPStubsResponse(fileAtPath:OHPathForFile("user.json", self.dynamicType)!,
@@ -19,5 +16,12 @@ class APIStub {
                     statusCode:500, headers:["Content-Type":"application/json"])
                 }
         })
+    }
+
+    private func checkHost(request: NSURLRequest) -> Bool {
+        if request.URL != nil && request.URL!.host != nil && Router.baseURLString.hasSuffix(request.URL!.host!) {
+            return true
+        }
+        return false
     }
 }
