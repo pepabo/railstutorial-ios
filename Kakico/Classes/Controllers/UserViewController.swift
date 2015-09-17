0d6fbe3a4399a47d31683ce3110b91cc626bcb46
suzupy <suzuki.n@pepabo.com>
2015-09-17 10:32:30 +0900

    Add user.viewWillAppear

--- Kakico/Classes/Controllers/UserViewController.swift
+++ Kakico/Classes/Controllers/UserViewController.swift
@@ -14,6 +14,7 @@ class UserViewController: UITableViewController {
     // MARK: - View Events
 
     override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
         resetSeparatorStyle()
         SVProgressHUD.showWithMaskType(.Black)
         users.drop()
